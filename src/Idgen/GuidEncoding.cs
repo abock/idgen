0a8433a95598adcefaea6961437352f898e6513c
Aaron Bockover <aaron@abock.dev>
2024-12-11 17:57:15 -0500

    Update C# to latest standards

---
+++ src/Idgen/GuidEncoding.cs
@@ -0,0 +1,14 @@
+//
+// Author:
+//   Aaron Bockover <aaron@abock.dev>
+//
+// Copyright 2018-2024 Aaron Bockover.
+// Licensed under the MIT License.
+
+namespace Idgen;
+
+public enum GuidEncoding
+{
+    BigEndian,
+    MixedEndian
+}
---
+++ src/Idgen/GuidFormat.cs
@@ -0,0 +1,19 @@
+//
+// Author:
+//   Aaron Bockover <aaron@abock.dev>
+//
+// Copyright 2018-2024 Aaron Bockover.
+// Licensed under the MIT License.
+
+namespace Idgen;
+
+public enum GuidFormat
+{
+    D,
+    N,
+    B,
+    P,
+    X,
+    Base64,
+    Short
+}
